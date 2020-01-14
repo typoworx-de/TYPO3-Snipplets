4c3aec17372758cd85b6bf4010406c1ea651e501
typoworx-de <info@typoworx.com>
2020-01-14 09:55:25 +0100

    Create LogDelegatorInterface.php

---
+++ Classes/Traits/LogDelegatorInterface.php
@@ -0,0 +1,15 @@
+<?php
+namespace Typoworx\FooBar\Traits;
+
+/**
+ * Class LogDelegatorInterface
+ */
+class LogDelegatorInterface
+{
+    const LOG_SUCCESS = 'success';
+    const LOG_INFO = 'info';
+    const LOG_NOTICE = 'notice';
+    const LOG_WARNING = 'warning';
+    const LOG_ERROR = 'error';
+    const LOG_DEBUG = 'debug';
+}
