aaa832135f39591c1a065327edfc9cca7f472ed2
typoworx-de <info@typoworx.com>
2020-01-14 09:42:16 +0100

    Update NamespaceUtility.php

--- Classes/Utility/NamespaceUtility.php
+++ Classes/Utility/NamespaceUtility.php
@@ -1,16 +1,15 @@
 <?php
-namespace Typoworx\Foobar\Utility;
+namespace Typoworx\FooBar\Utility;
 
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use Composer\Autoload\ClassLoader;
 
 /**
  * Class NamespaceUtility
- * @package Typoworx\Foobar\Utility
  */
 class NamespaceUtility
 {
     /**
-     * @param string $namespace         Class-Namespace        (optional, if unset auto-detecting)
+     * @param string $namespace     Class-Namespace (optional, if unset auto-detecting)
      * @return array|null
      */
     public static function getNamespace(string $namespace = __NAMESPACE__)
@@ -20,24 +19,12 @@ class NamespaceUtility
         if(!isset($cachedNamespaces[ $namespace ]))
         {
             $namespaceParts = [];
-            if(
-                strpos($namespace, '\\Controller\\') !== false &&
-                preg_match_all('~(?<vendor>[^\\\]+)\\\(?<extension>[^\\\]+)\\\(Controller\\\(?<controller>[^\\\]+))?~', $namespace, $namespaceParts))
+            if(preg_match_all('~(?<vendor>[^\\\]+)\\\(?<extension>[^\\\]+)\\\(Controller\\\(?<controller>[^\\\]+))?~', $namespace, $namespaceParts))
             {
                 $cachedNamespaces[ $namespace ] = [
                     'vendor' => isset($namespaceParts['vendor'][0]) ? $namespaceParts['vendor'][0] : null,
                     'extension' => isset($namespaceParts['extension'][0]) ? $namespaceParts['extension'][0] : null,
                     'controller' => isset($namespaceParts['controller'][0]) ? $namespaceParts['controller'][0] : null,
-                ];
-            }
-            else             if(
-                strpos($namespace, '\\Domain\\Model\\') !== false &&
-                preg_match_all('~(?<vendor>[^\\\]+)\\\(?<extension>[^\\\]+)\\\Domain\\\Model\\\(?<model>[^\\\]+)?~', $namespace, $namespaceParts))
-            {
-                $cachedNamespaces[ $namespace ] = [
-                    'vendor' => isset($namespaceParts['vendor'][0]) ? $namespaceParts['vendor'][0] : null,
-                    'extension' => isset($namespaceParts['extension'][0]) ? $namespaceParts['extension'][0] : null,
-                    'model' => isset($namespaceParts['model'][0]) ? $namespaceParts['model'][0] : null,
                 ];
             }
             else
@@ -63,5 +50,71 @@ class NamespaceUtility
         }
 
         return $namespace;
+    }
+
+    /**
+     * @param string $namespace
+     * @return string|null
+     */
+    public static function getVendorName(string $namespace) :? string
+    {
+        $namespace = self::getNamespace($namespace);
+
+        return isset($namespace['vendor'])
+            ? $namespace['vendor']
+            : null
+            ;
+    }
+
+    /**
+     * @param string $namespace
+     * @return string|null
+     */
+    public static function getExtensionKey(string $namespace) :? string
+    {
+        $namespace = self::getNamespace($namespace);
+
+        return isset($namespace['extension'])
+            ? $namespace['extension']
+            : null
+        ;
+    }
+
+    /**
+     * @param string $extensionKey
+     * @return string|null
+     * @throws \TYPO3\CMS\Core\Exception
+     */
+    protected function getExtensionNamespacePrefix(string $extensionKey) :? string
+    {
+        static $extensionKeys;
+
+        if($extensionKeys === null)
+        {
+            $extensionKeys = [];
+        }
+
+        $extensionKey = trim($extensionKey);
+        if(empty($extensionKey))
+        {
+            return '';
+        }
+
+        if(isset($extensionKeys[ $extensionKey ]))
+        {
+            return $extensionKeys[ $extensionKey ];
+        }
+
+        /** @var ClassLoader $classLoader */
+        $classLoader = Bootstrap::getInstance()->getEarlyInstance(ClassLoader::class);
+
+        $result = array_filter($classLoader->getPrefixesPsr4(), function($item) use($extensionKey) {
+            return strpos(current($item), $extensionKey) !== false;
+        });
+
+        $vendorPrefix = !empty($result) ? trim(key($result), '\ ') : null;
+        $extensionKeys[ $extensionKey ] = $vendorPrefix;
+
+        return $extensionKeys[ $extensionKey ];
     }
 }
