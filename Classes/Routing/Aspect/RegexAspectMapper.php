302a0b97e42d8f7031b0877f62584d9bf7bb423b
typoworx-de <info@typoworx.com>
2020-07-07 14:12:33 +0200

    Create RegexAspectMapper.php

---
+++ Classes/Routing/Aspect/RegexAspectMapper.php
@@ -0,0 +1,81 @@
+<?php
+namespace Foo\FooBar\Routing\Aspect;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Routing\Aspect\StaticMappableAspectInterface;
+
+/**
+ * Class RegexAspectMapper
+ * @package Foo\Foo\Routing\Aspect
+ */
+class RegexAspectMapper implements StaticMappableAspectInterface
+{
+    /**
+     * @var string
+     */
+    protected $regexPattern;
+
+
+    /**
+     * @param array $settings
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $settings)
+    {
+        $this->regexPattern = !isset($settings['regex']) ?: $settings['regex'];
+    }
+
+    /**
+     * @param string $value
+     * @return string|null
+     * @throws \Throwable only in Development-Mode
+     */
+    public function generate(string $value): ?string
+    {
+        return $this->isPatternValid($value) ? $value : null;
+    }
+
+    /**
+     * @param string $value
+     * @return string|null
+     * @throws \Throwable only in Development-Mode
+     */
+    public function resolve(string $value): ?string
+    {
+        return $this->isPatternValid($value) ? $value : null;
+    }
+
+    /**
+     * @param string $value
+     * @return bool
+     * @throws \Throwable only in Development-Mode
+     */
+    protected function isPatternValid(string $value) : bool
+    {
+        $return = false;
+
+        try
+        {
+            $pattern = $this->regexPattern;
+
+            if(substr($pattern, 0) !== substr($pattern, -1))
+            {
+                if(in_array(substr($pattern, 0), ['!', '^', '(', '$']))
+                {
+                    $pattern = '~' . $pattern . '~';
+                }
+            }
+
+            $return = preg_match($pattern, $value);
+        }
+        catch (\Throwable $e)
+        {
+            if(!GeneralUtility::getApplicationContext()->isProduction())
+            {
+                throw $e;
+            }
+        }
+
+        return $return;
+    }
+}
