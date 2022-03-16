bc9552f2ce0cf0390edfb430507456a363f0557b
typoworx-de <info@typoworx.com>
2022-03-16 17:00:00 +0100

    Create TestListener.php

---
+++ Tests/Unit/Base/TestListener.php
@@ -0,0 +1,44 @@
+<?php
+namespace Foo\Bar\Tests\Base;
+
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+
+class TestListener implements \PHPUnit\Framework\TestListener
+{
+    public function startTestSuite(TestSuite $suite) : void
+    {
+        if($suite->getName() === 'functional')
+        {
+        }
+    }
+
+    public function addError(Test $test, \Throwable $t, float $time) : void
+    {}
+
+    public function addWarning(Test $test, Warning $e, float $time) : void
+    {}
+
+    public function addFailure(Test $test, AssertionFailedError $e, float $time) : void
+    {}
+
+    public function addIncompleteTest(Test $test, \Throwable $t, float $time) : void
+    {}
+
+    public function addRiskyTest(Test $test, \Throwable $t, float $time) : void
+    {}
+
+    public function addSkippedTest(Test $test, \Throwable $t, float $time) : void
+    {}
+
+    public function endTestSuite(TestSuite $suite) : void
+    {}
+
+    public function startTest(Test $test) : void
+    {}
+
+    public function endTest(Test $test, float $time) : void
+    {}
+}
